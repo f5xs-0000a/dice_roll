1c784c9523052758b835d8d95bf52445c68576f8
F5XS <f5xs.0000a@gmail.com>
2023-08-06 10:23:10 +0800

    better way to write

--- src/main.rs
+++ src/main.rs
@@ -27,28 +27,35 @@ fn roll_dice(sizes: &[usize]) {
     let mut rolls = Vec::with_capacity(sizes.len());
     let one_second = Duration::from_secs(1);
     let mut stdout = std::io::stdout().lock();
+    let mut buffer = String::new();
 
     let now = Instant::now();
 
     while now.elapsed() < one_second {
         rolls.clear();
+        buffer.clear();
 
+        // roll dice
         for die in sizes.iter() {
             // you can do a better randomizer here
             rolls.push(random::<usize>() % die + 1);
         }
 
-        write_dice_faces(&*rolls, &mut stdout).unwrap();
+        write_dice_faces(&*rolls, &mut buffer).unwrap();
+
+        // write to stdout and flush
+        write!(&mut stdout, "\x1B[2J\x1B[1;1H{}", buffer);
+        stdout.flush();
     }
 }
 
 fn write_dice_faces(
     rolls: &[usize],
-    w: &mut impl Write,
-) -> std::io::Result<()> {
+    w: &mut impl core::fmt::Write,
+) -> Result<(), core::fmt::Error> {
     for roll in rolls.iter() {
         write!(w, "{}  ", roll)?;
     }
 
-    w.flush()
+    Ok(())
 }
